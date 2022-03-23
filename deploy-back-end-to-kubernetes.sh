a90cbd2f1f734725c8292ca72aa00fa46bc1998e
yaskovdev <yaskovdev@gmail.com>
2022-03-23 21:32:59 +0200

    Use Git revision to tag images to make sure every image is unique. This will allow to rollout updates without deployment recreation.

--- deploy-back-end-to-kubernetes.sh
+++ deploy-back-end-to-kubernetes.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-docker image build -t yaskovdev/motive-back-end -f ../motive-back-end/Dockerfile ../motive-back-end
-docker image push yaskovdev/motive-back-end
+TAG=$(git -C ../motive-back-end rev-parse --short HEAD)
+IMAGE=yaskovdev/motive-back-end:$TAG
+docker image build -t "$IMAGE" -f ../motive-back-end/Dockerfile ../motive-back-end
+docker image push "$IMAGE"
 
+export FULL_IMAGE=docker.io/$IMAGE
 export APP_NAMESPACE_NAME="motive"
 export APP_DATABASE_NAME="motive-database"
 export DATABASE_JDBC_URL="jdbc:postgresql://$APP_DATABASE_NAME-postgresql.motive.svc.cluster.local:5432/motive"
